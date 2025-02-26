2ca5b6a00a4ac16bdc9dbe5b4c475ca2c6a9d7a8
lfuw <lucas@upowl.com.br>
2025-02-25 23:05:11 -0300

    feat: adicionando gin

--- cmd/connector/main.go
+++ cmd/connector/main.go
@@ -1,61 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/qri-io/jsonschema"
+	"github.com/yrn-go/yrn/pkg/yconnector"
+	"golang.org/x/exp/slog"
 	"log"
-	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/hashicorp/consul/api"
 )
-
-var (
-	EnvServiceName = os.Getenv("SERVICE_NAME")
-	EnvServiceHost = os.Getenv("SERVICE_HOST")
-	EnvServicePort = os.Getenv("SERVICE_PORT")
-)
-
-func registerService() {
-	client, err := api.NewClient(api.DefaultConfig())
-	if err != nil {
-		log.Fatalf("Erro ao conectar ao Consul: %v", err)
-	}
-
-	servicePort, _ := strconv.Atoi(EnvServicePort)
-
-	registration := &api.AgentServiceRegistration{
-		ID:      EnvServiceName,
-		Name:    EnvServiceName,
-		Address: EnvServiceHost,
-		Port:    servicePort,
-		Check: &api.AgentServiceCheck{
-			HTTP:     fmt.Sprintf("http://%s:%d/health", EnvServiceHost, servicePort),
-			Interval: "10s",
-		},
-		Meta: map[string]string{},
-	}
-
-	err = client.Agent().ServiceRegister(registration)
-	if err != nil {
-		log.Fatalf("Erro ao registrar serviço no Consul: %v", err)
-	}
-
-	log.Printf("Serviço %s registrado no Consul", EnvServiceName)
-}
 
 func main() {
-	registerService()
+	var schema jsonschema.Schema
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		_, _ = fmt.Fprintln(w, "OK")
-	})
+	if err := json.Unmarshal([]byte(schemaData), &schema); err != nil {
+		log.Panicf("unmarshal schema: %v\n", err.Error())
+	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintln(w, fmt.Sprintf("Hello from %s!", EnvServiceName))
-	})
-
-	log.Printf("%s rodando na porta %s", EnvServiceName, EnvServicePort)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", EnvServicePort), nil))
+	appRun := yconnector.NewApp(&schema, map[string]string{})
+	if err := appRun(); err != nil {
+		slog.Error("server error: ", slog.Any("error", err))
+	}
 }
+
+const schemaData = `{
+	"$id": "https://qri.io/schema/",
+	"$comment" : "sample comment",
+	"title": "Person",
+	"type": "object",
+	"properties": {
+		"firstName": {
+			"type": "string"
+		},
+		"lastName": {
+			"type": "string"
+		},
+		"age": {
+			"description": "Age in years",
+			"type": "integer",
+			"minimum": 0
+		},
+		"friends": {
+			"type" : "array",
+			"items" : { "title" : "REFERENCE", "$ref" : "#" }
+		}
+	},
+	"required": ["firstName", "lastName"]
+}`
--- pkg/yconnector/app.go
+++ pkg/yconnector/app.go
@@ -1,1 +1,96 @@
 package yconnector
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/consul/api"
+	"github.com/qri-io/jsonschema"
+	"golang.org/x/exp/slog"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+const (
+	EnvServiceName = "SERVICE_NAME"
+	EnvServiceHost = "SERVICE_HOST"
+	EnvServicePort = "SERVICE_PORT"
+
+	EndpointHealth = "/health"
+	EndpointSchema = "/schema"
+)
+
+var (
+	serviceName = os.Getenv(EnvServiceName)
+	serviceHost = os.Getenv(EnvServiceHost)
+	servicePort = os.Getenv(EnvServicePort)
+)
+
+type ServerRunFunc func() (err error)
+
+func NewApp(
+	schema *jsonschema.Schema,
+	meta map[string]string,
+) ServerRunFunc {
+	switch "" {
+	case
+		serviceName,
+		serviceHost,
+		servicePort,
+		os.Getenv(api.HTTPAddrEnvName):
+		log.Panicf("envs SERVICE_NAME, SERVICE_HOST, SERVICE_PORT and %s is required\n", api.HTTPAddrEnvName)
+	}
+
+	switch nil {
+	case schema:
+		log.Panicln("schema is required")
+	}
+
+	registerService(meta)
+
+	engine := gin.Default()
+
+	engine.GET(EndpointHealth, func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	engine.GET(EndpointSchema, func(c *gin.Context) {
+		c.JSON(http.StatusOK, schema)
+	})
+
+	return func() (err error) {
+		return engine.Run(":" + servicePort)
+	}
+}
+
+func registerService(meta map[string]string) {
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Panicf("error connecting to Consul: %v\n", err)
+	}
+
+	servicePortInt, _ := strconv.Atoi(servicePort)
+
+	registration := &api.AgentServiceRegistration{
+		ID:      serviceName,
+		Name:    serviceName,
+		Address: serviceHost,
+		Port:    servicePortInt,
+		Check: &api.AgentServiceCheck{
+			HTTP:     fmt.Sprintf("http://%s:%s/health", serviceHost, servicePort),
+			Interval: "10s",
+		},
+		Meta: meta,
+	}
+
+	err = client.Agent().ServiceRegister(registration)
+	if err != nil {
+		log.Panicf("error registering service in Consul: %v\n", err)
+	}
+
+	slog.Info(
+		"service registered in Consul",
+		slog.String("serviceName", serviceName),
+	)
+}
