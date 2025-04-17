fa80c9ca442d772f1c76fb3e0ff58d277a557393
lfuw <lucas@upowl.com.br>
2025-04-17 03:28:39 -0300

    feat: configurando deployment para o argocd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "golang.org/x/exp/slog"
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/exp/slog"
+	"net/http"
+)
 
 func main() {
-	
 	slog.Info("start api")
+
+	engine := gin.Default()
+
+	engine.GET("/health", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
+	if err := engine.Run(); err != nil {
+		panic(err)
+	}
 }
